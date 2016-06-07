fb891291799b6a15768921acf5b2954ff95ab5a4
MDaems <micky_daems@hotmail.com>
2016-06-07 12:39:25 +0200

    SASS themes

--- app/Controllers/HomeController.js
+++ app/Controllers/HomeController.js
@@ -2,7 +2,6 @@ module.exports = function($scope){
 	$scope.token = window.localStorage['token'];
 
 	var self = this;
-	$scope.myDynamicClass = 'banana';
 
 	self.changeTheme = function(theme) {
 		$scope.myDynamicClass = theme;
